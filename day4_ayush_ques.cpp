c1a8f76ad270ddbb47fd875ec731d59df3ca77b2
Roushan verma <roushanverma123@gmail.com>
2019-02-25 22:55:32 +0530

    added day 4 question by ayush in c++

---
+++ day4_ayush_ques.cpp
@@ -0,0 +1,30 @@
+//link to question  
+//https://www.hackerrank.com/challenges/c-tutorial-struct/problem
+
+
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+/*
+    add code for struct here.
+*/
+struct Student{
+    int age,standard;
+    char first_name[51],last_name[51];
+
+};
+
+int main() {
+    Student st;
+    
+    cin >> st.age >> st.first_name >> st.last_name >> st.standard;
+    cout << st.age << " " << st.first_name << " " << st.last_name << " " << st.standard;
+    
+    return 0;
+}
+
+
